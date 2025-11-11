7d2c7b4c22558c75a8f1e24214e4f621822167c0
Betan207 <mordillo1007@gmail.com>
2025-11-11 12:49:26 +0100

    Añadido mas coasa

--- src/main/java/org/yourcompany/calculadora/Calculadora.java
+++ src/main/java/org/yourcompany/calculadora/Calculadora.java
@@ -4,38 +4,39 @@ import java.util.Scanner;
 
 /**
  *
- * @author Posa el teu nom
+ * @author Andreu
  */
 
 public class Calculadora {
 
     public static void main(String[] args) {
+        char respuesta;
         Scanner teclado = new Scanner(System.in);
         int opcion;
         System.out.println("Benvingut, escolleix quina operacio vol executar. Te 7 opcions");
         do{
-            System.out.println("Menu de opcions:");
-            System.out.println("1. Nombre de dígits d'un nombre");
-            System.out.println("2. Llançament de moneda");
-            System.out.println("3. Càlcul preu entrada cinema");
-            System.out.println("4. Suma dels primers N números");
-            System.out.println("5. Càlcul factorial d'un nombre");
-            System.out.println("6. Suma dels quadrats dels primers N números");
-            System.out.println("7. Càlcul de potència d'un nombre");
+            MostrarMenu(args);
             opcion = teclado.nextInt();
 
             switch (opcion) {
 
             case 1 -> {
+                
+                do{
                 // Exemples de crida per al mètode nombreDigits
                 System.out.println("Nombre de digits de 1234: " + nombreDigits(1234));
                 System.out.println("Nombre de digits de 56789: " + nombreDigits(56789));
                 System.out.println("Nombre de digits de -101010: " + nombreDigits(-101010));
                 System.out.println("Nombre de digits de 0: " + nombreDigits(0));
+                System.out.println("Vols repetir aquesta operacio? (s/n): ");
+                respuesta = teclado.next().charAt(0);
+                }while(respuesta == 's' || respuesta == 'S');
+
                 }
 
             case 2 -> {
                 // Codi per a llançar la moneda
+                do{
                 int repeticions; // Nombre de llançaments
                 System.out.print("Introdueix el nombre de llansaments: ");
                 repeticions = teclado.nextInt();
@@ -43,49 +44,90 @@ public class Calculadora {
                 System.out.println("Nombre de cares en " + repeticions + " llansaments: " + numeroCares);
                 int numerocruces = repeticions - numeroCares;
                 System.out.println("Nombre de cruces en " + repeticions + " llansaments: " + numerocruces);
+                System.out.println("Vols repetir aquesta operacio? (s/n): ");
+                respuesta = teclado.next().charAt(0);
+                }while(respuesta == 's' || respuesta == 'S');
+
                 }
 
             case 3 -> {
+                do{
                 //Tornada de preu entrada cinema
                 double preuBase = 8.0; // Preu base de l'entrada
                 boolean esCapDeSetmana = true; // Exemple: és cap de setmana
                 boolean teCarnetJove = false; // Exemple: no té Carnet Jove    
                 double preuFinal = CalculadoraPreuEntrada(preuBase, esCapDeSetmana, teCarnetJove);
                 System.out.println("Preu final de l'entrada: " + preuFinal);
+                System.out.println("Vols repetir aquesta operacio? (s/n): ");
+                respuesta = teclado.next().charAt(0);
+                }while(respuesta == 's' || respuesta == 'S');
+
                 }
 
             case 4 -> {
+                do{
                 // Exemples de crida per al mètode sumaPrimersNumeros
                 System.out.println("Suma dels primers 5 numeros: " + sumaPrimersNumeros(5));
                 System.out.println("Suma dels primers 10 numeros: " + sumaPrimersNumeros(10));
                 System.out.println("Suma dels primers 15 numeros: " + sumaPrimersNumeros(15));
+                System.out.println("Vols repetir aquesta operacio? (s/n): ");
+                respuesta = teclado.next().charAt(0);
+                }while(respuesta == 's' || respuesta == 'S');
+
                 }
 
             case 5 -> {
+                do{
                 // Exemples de crida per al mètode calcularFactorial
                 System.out.println("Factorial de 5: " + calcularFactorial(5));
                 System.out.println("Factorial de 7: " + calcularFactorial(7));
                 System.out.println("Factorial de 10: " + calcularFactorial(10));
+                System.out.println("Vols repetir aquesta operacio? (s/n): ");
+                respuesta = teclado.next().charAt(0);
+                }while(respuesta == 's' || respuesta == 'S');
+
                 }
 
             case 6 -> {
+                do{
                 // Exemples de crida per al mètode sumaQuadrats
                 System.out.println("Suma dels quadrats dels primers 3 numeros: " + sumaQuadrats(3));
                 System.out.println("Suma dels quadrats dels primers 5 numeros: " + sumaQuadrats(5));
                 System.out.println("Suma dels quadrats dels primers 7 numeros: " + sumaQuadrats(7));
+                System.out.println("Vols repetir aquesta operacio? (s/n): ");
+                respuesta = teclado.next().charAt(0);
+                }while(respuesta == 's' || respuesta == 'S');
+
                 }
 
             case 7 -> {
+                do{
                 // Exemples de crida per al mètode calcularPotencia
                 System.out.println("2 elevat a la potencia 3: " + calcularPotencia(2, 3));
                 System.out.println("5 elevat a la potencia 4: " + calcularPotencia(5, 4));
                 System.out.println("3 elevat a la potencia 5: " + calcularPotencia(3, 5));
+                System.out.println("Vols repetir aquesta operacio? (s/n): ");
+                respuesta = teclado.next().charAt(0);
+                }while(respuesta == 's' || respuesta == 'S');
+
                 }
 
             default -> System.out.println("Opcio no valida");
             }
         
-        }while(opcion<8);
+        }while(opcion!=0);
+    }
+    public static void MostrarMenu(String[] args) {
+        System.out.println("Menu de opcions:");
+            System.out.println("1. Nombre de dígits d'un nombre");
+            System.out.println("2. Llançament de moneda");
+            System.out.println("3. Càlcul preu entrada cinema");
+            System.out.println("4. Suma dels primers N números");
+            System.out.println("5. Càlcul factorial d'un nombre");
+            System.out.println("6. Suma dels quadrats dels primers N números");
+            System.out.println("7. Càlcul de potència d'un nombre");
+            System.out.println("0. Sortir");
+        
     }
 
     public static int sumaPrimersNumeros(int n) {
